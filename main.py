7759eba077926ea1e9bbfb741153ce3d784fb4a7
Phil Higdon <philhigdon@sky.com>
2024-05-07 14:58:35 +0100

    added actor and crew details

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import contextlib
 from datetime import datetime, timedelta
 from flask import Flask, render_template, request, redirect, url_for, session
 from flask_caching import Cache
@@ -11,49 +12,49 @@ load_dotenv()
 
 # Initialize Flask app
 app = Flask(__name__)
-app.secret_key = os.getenv('FLASK_SECRET_KEY')  # Use a fallback if not set
+app.secret_key = os.getenv("FLASK_SECRET_KEY")  # Use a fallback if not set
 Bootstrap(app)
 
 # Setup cache
-cache = Cache(app, config={'CACHE_TYPE': 'simple'})
+cache = Cache(app, config={"CACHE_TYPE": "simple"})
 
 # API Key and base URL from TMDB
 API_KEY = os.getenv("TMDB_API_KEY")
 TMDB_URL = "https://api.themoviedb.org/3/"
 
-@app.route('/', methods=['GET', 'POST'])
-@cache.cached(timeout=3600, query_string=True)
 
+@app.route("/", methods=["GET", "POST"])
+@cache.cached(timeout=3600, query_string=True)
 def home():
-    page = request.args.get('page', 1, type=int)
-    region = request.args.get('region', default='GB')
+    page = request.args.get("page", 1, type=int)
+    region = request.args.get("region", default="GB")
     today_date = datetime.now()
     two_years_later = today_date + timedelta(days=730)
 
-    date_today_str = today_date.strftime('%Y-%m-%d')
-    date_two_years_str = two_years_later.strftime('%Y-%m-%d')
+    date_today_str = today_date.strftime("%Y-%m-%d")
+    date_two_years_str = two_years_later.strftime("%Y-%m-%d")
 
     params = {
-        'api_key': API_KEY,
-        'primary_release_date.gte': date_today_str,
-        'primary_release_date.lte': date_two_years_str,
-        'region': region,
-        'language': 'en-GB',
-        'page': page,
-        'append_to_response': 'details,videos'
+        "api_key": API_KEY,
+        "primary_release_date.gte": date_today_str,
+        "primary_release_date.lte": date_two_years_str,
+        "region": region,
+        "language": "en-GB",
+        "page": page,
+        "append_to_response": "details,videos",
     }
     response = requests.get(f"{TMDB_URL}discover/movie", params=params)
     if response.status_code == 200:
         data = response.json()
-        if 'results' in data:
-            movies = data['results']
+        if "results" in data:
+            movies = data["results"]
 
             # Custom sorting function
             def sort_by_release_date(movie):
-                release_date = movie.get('release_date')
+                release_date = movie.get("release_date")
                 if release_date:
                     try:
-                        return datetime.strptime(release_date, '%Y-%m-%d')
+                        return datetime.strptime(release_date, "%Y-%m-%d")
                     except ValueError:
                         # Handle invalid date format
                         return datetime.min
@@ -65,51 +66,79 @@ def home():
 
             # Format release dates in UK format
             for movie in movies:
-                release_date = movie.get('release_date')
+                release_date = movie.get("release_date")
                 if release_date:
-                    try:
-                        date_obj = datetime.strptime(release_date, '%Y-%m-%d')
-                        movie['release_date'] = date_obj.strftime('%d/%m/%Y')
-                    except ValueError:
-                        # Handle invalid date format
-                        pass
-
-            total_pages = data['total_pages']
-            return render_template('index.html', movies=movies, region=region, page=page, total_pages=total_pages)
+                    with contextlib.suppress(ValueError):
+                        date_obj = datetime.strptime(release_date, "%Y-%m-%d")
+                        movie["release_date"] = date_obj.strftime("%d/%m/%Y")
+            total_pages = data["total_pages"]
+            return render_template(
+                "index.html",
+                movies=movies,
+                region=region,
+                page=page,
+                total_pages=total_pages,
+            )
         else:
             error_message = "No results found."
     else:
         error_message = f"Failed to fetch data: {response.status_code}"
 
-    return render_template('index.html', error=error_message, region=region, page=page)
+    return render_template("index.html", error=error_message, region=region, page=page)
 
-@app.route('/movie/<int:movie_id>')
+
+@app.route("/movie/<int:movie_id>")
 def movie_details(movie_id):
-    params = {
-        'api_key': API_KEY,
-        'append_to_response': 'videos,credits,reviews'
-    }
+    params = {"api_key": API_KEY, "append_to_response": "videos,credits,reviews"}
     response = requests.get(f"{TMDB_URL}movie/{movie_id}", params=params)
     if response.status_code != 200:
-        return render_template('error.html', message="Failed to fetch movie details.")
+        return render_template("error.html", message="Failed to fetch movie details.")
     movie_info = response.json()
-    return render_template('movie_details.html', movie=movie_info)
+    return render_template("movie_details.html", movie=movie_info)
 
-@app.route('/search', methods=['GET', 'POST'])
+
+@app.route("/search", methods=["GET", "POST"])
 def search():
     if request.method == "POST":
-        query = request.form['search']
-        params = {
-            'api_key': API_KEY,
-            'query': query,
-            'include_adult': 'false'
-        }
+        query = request.form["search"]
+        params = {"api_key": API_KEY, "query": query, "include_adult": "false"}
         response = requests.get(f"{TMDB_URL}search/movie", params=params)
         if response.status_code != 200:
-            return render_template('search.html', error="Failed to retrieve results.")
-        search_results = response.json()['results']
-        return render_template('search.html', movies=search_results)
-    return render_template('search.html')
+            return render_template("search.html", error="Failed to retrieve results.")
+        search_results = response.json()["results"]
+        return render_template("search.html", movies=search_results)
+    return render_template("search.html")
+
+
+@app.route("/actor_details/<int:actor_id>/<int:movie_id>")
+def actor_details(actor_id, movie_id):
+    params = {
+        "api_key": API_KEY,
+        "append_to_response": "movie_credits,external_ids",  # Include external_ids
+    }
+    response = requests.get(f"{TMDB_URL}person/{actor_id}", params=params)
+    if response.status_code == 200:
+        actor_info = response.json()
+        return render_template(
+            "actor_details.html", actor=actor_info, movie_id=movie_id
+        )
+    else:
+        return render_template("error.html", message="Failed to fetch actor details.")
+
+
+@app.route("/crew_details/<int:crew_id>/<int:movie_id>")
+def crew_details(crew_id, movie_id):
+    params = {
+        "api_key": API_KEY,
+        "append_to_response": "movie_credits",  # Ensure movie credits are included
+    }
+    response = requests.get(f"{TMDB_URL}person/{crew_id}", params=params)
+    if response.status_code == 200:
+        crew_info = response.json()
+        return render_template("crew_details.html", crew=crew_info, movie_id=movie_id)
+    else:
+        return render_template("error.html", message="Failed to fetch crew details.")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
